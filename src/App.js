1c82a7ac6b4d1e8961c76daedd7ac0a5351dd93e
meroz <ermamone@gmail.com>
2022-05-11 20:45:07 -0300

    Se crea archivo Header para los body

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import IndexPage from './Pages/IndexPage';
 
+var context;
+
 function App() {
   return (
     <div className="App">
-      <IndexPage/>
+        <Header params={context} className="Heaher-Context"/>
+        <IndexPage params={context} className="Body-Context"/>
     </div>
   );
 }
---
+++ src/Pages/Header.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+
+function Header(params){
+    params = "hola mundo"
+    return params;
+}
+
+export default Header;
